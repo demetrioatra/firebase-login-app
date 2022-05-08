01ce7537893646b04f212780f34a956c3df2087b
Demetrio <dema.atra1@gail.com>
2022-05-08 17:52:17 -0300

    register logic

--- app/src/main/java/com/devd/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/devd/projetofirebase/FormCadastro.java
@@ -6,9 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
@@ -16,7 +14,6 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -25,29 +22,30 @@ public class FormCadastro extends AppCompatActivity {
 
     private EditText editName, editEmail, editPassword;
     private Button button;
-    String[] messages = {"Preencha todos os campos!", "Cadastro realizado com Sucesso!"};
+    String[] messages = { "Preencha todos os campos!", "Cadastro realizado com Sucesso!" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
-
         Objects.requireNonNull(getSupportActionBar()).hide();
+
         setComponents();
 
         button.setOnClickListener(view -> {
+
             String name = editName.getText().toString();
             String email = editEmail.getText().toString();
             String password = editPassword.getText().toString();
 
-            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty())
+            {
                 Snackbar snackbar = Snackbar.make(view, messages[0], Snackbar.LENGTH_SHORT);
                 snackbar.setBackgroundTint(Color.WHITE);
                 snackbar.setTextColor(Color.BLACK);
                 snackbar.show();
-            } else {
-                registerUser(view);
             }
+            else register(view);
         });
     }
 
@@ -58,7 +56,8 @@ public class FormCadastro extends AppCompatActivity {
         button = findViewById(R.id.btn_register);
     }
 
-    private void registerUser(View view) {
+    private void register(View view) {
+
         String email = editEmail.getText().toString();
         String password = editPassword.getText().toString();
 
@@ -71,19 +70,17 @@ public class FormCadastro extends AppCompatActivity {
                 snackbar.setBackgroundTint(Color.WHITE);
                 snackbar.setTextColor(Color.BLACK);
                 snackbar.show();
+
             } else {
+
                 String error;
-                try {
-                    throw Objects.requireNonNull(task.getException());
-                } catch (FirebaseAuthWeakPasswordException e) {
-                    error = "Digite uma senha de, no mínimo, 6 caracteres";
-                } catch (FirebaseAuthUserCollisionException e) {
-                    error = "Esta conta ja foi cadastrada";
-                } catch (FirebaseAuthInvalidCredentialsException e) {
-                    error = "E-mail inválido";
-                } catch (Exception e) {
-                    error = "Erro ao cadastrar usuário";
-                }
+
+                try                                                 { throw Objects.requireNonNull(task.getException()); }
+                catch (FirebaseAuthWeakPasswordException e)         { error = "Digite uma senha de, no mínimo, 6 caracteres"; }
+                catch (FirebaseAuthUserCollisionException e)        { error = "Esta conta ja foi cadastrada"; }
+                catch (FirebaseAuthInvalidCredentialsException e)   { error = "E-mail inválido"; }
+                catch (Exception e)                                 { error = "Erro ao cadastrar usuário"; }
+
                 Snackbar snackbar = Snackbar.make(view, error, Snackbar.LENGTH_SHORT);
                 snackbar.setBackgroundTint(Color.WHITE);
                 snackbar.setTextColor(Color.BLACK);
@@ -93,16 +90,16 @@ public class FormCadastro extends AppCompatActivity {
     }
 
     private void saveUser() {
+
         String name = editName.getText().toString();
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+        String userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
         Map<String, Object> users = new HashMap<>();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
         users.put("name", name);
 
-        String userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        DocumentReference documentReference = firestore.collection("Users").document(userID);
 
-        DocumentReference documentReference = db.collection("Users").document(userID);
         documentReference.set(users)
                 .addOnSuccessListener(unused -> Log.d("db", "Sucesso ao salvar dados"))
                 .addOnFailureListener(e -> Log.d("db_error", "Erro ao salvar dados" + e));
--- app/src/main/java/com/devd/projetofirebase/FormLogin.java
+++ app/src/main/java/com/devd/projetofirebase/FormLogin.java
@@ -1,16 +1,28 @@
 package com.devd.projetofirebase;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
 
-    private TextView txt_cadastro;
+    private EditText editEmail, editPassword;
+    private Button button;
+    private TextView textCadastro;
+    private ProgressBar progressBar;
+    String[] messages = { "Preencha todos os campos!" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +32,40 @@ public class FormLogin extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         setComponents();
 
-        txt_cadastro.setOnClickListener(view -> {
-            Intent intent = new Intent(FormLogin.this, FormCadastro.class);
-            startActivity(intent);
+        button.setOnClickListener(view -> {
+
+            String email = editEmail.getText().toString();
+            String password = editPassword.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty())
+            {
+                Snackbar snackbar = Snackbar.make(view, messages[0], BaseTransientBottomBar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            } else authenticate();
+        });
+
+        textCadastro.setOnClickListener(view -> startActivity(new Intent(FormLogin.this, FormCadastro.class)));
+    }
+
+    private void authenticate() {
+
+        String email = editEmail.getText().toString();
+        String password = editPassword.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+
+            }
         });
     }
 
     private void setComponents() {
-        txt_cadastro = findViewById(R.id.txt_cadastro);
+        textCadastro = findViewById(R.id.txt_cadastro);
+        editEmail = findViewById(R.id.edt_email);
+        editPassword = findViewById(R.id.edt_password);
+        button = findViewById(R.id.btn_entrar);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
