a6b32cd7dd22479d54d7b68e8d58e6ec9a420e89
Demétrio <dema.atra1@gmail.com>
2022-05-08 20:59:42 -0300

    login logic

--- app/src/main/java/com/devd/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/devd/projetofirebase/FormCadastro.java
@@ -32,65 +32,69 @@ public class FormCadastro extends AppCompatActivity {
 
         setComponents();
 
-        button.setOnClickListener(view -> {
-
+        button.setOnClickListener(view ->
+        {
             String name = editName.getText().toString();
             String email = editEmail.getText().toString();
             String password = editPassword.getText().toString();
 
             if (name.isEmpty() || email.isEmpty() || password.isEmpty())
             {
-                Snackbar snackbar = Snackbar.make(view, messages[0], Snackbar.LENGTH_SHORT);
-                snackbar.setBackgroundTint(Color.WHITE);
-                snackbar.setTextColor(Color.BLACK);
+                Snackbar snackbar = Snackbar
+                    .make(view, messages[0], Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(Color.WHITE)
+                    .setTextColor(Color.BLACK);
                 snackbar.show();
             }
             else register(view);
         });
     }
 
-    private void setComponents() {
+    private void setComponents()
+    {
         editName = findViewById(R.id.edt_name);
         editEmail = findViewById(R.id.edt_email);
         editPassword = findViewById(R.id.edt_password);
         button = findViewById(R.id.btn_register);
     }
 
-    private void register(View view) {
-
+    private void register(View view)
+    {
         String email = editEmail.getText().toString();
         String password = editPassword.getText().toString();
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task ->
+        {
+            if (task.isSuccessful())
+            {
                 saveUser();
 
-                Snackbar snackbar = Snackbar.make(view, messages[1], Snackbar.LENGTH_SHORT);
-                snackbar.setBackgroundTint(Color.WHITE);
-                snackbar.setTextColor(Color.BLACK);
+                Snackbar snackbar = Snackbar
+                    .make(view, messages[1], Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(Color.WHITE)
+                    .setTextColor(Color.BLACK);
                 snackbar.show();
-
-            } else {
-
+            }
+            else
+            {
                 String error;
-
                 try                                                 { throw Objects.requireNonNull(task.getException()); }
                 catch (FirebaseAuthWeakPasswordException e)         { error = "Digite uma senha de, no mínimo, 6 caracteres"; }
                 catch (FirebaseAuthUserCollisionException e)        { error = "Esta conta ja foi cadastrada"; }
                 catch (FirebaseAuthInvalidCredentialsException e)   { error = "E-mail inválido"; }
                 catch (Exception e)                                 { error = "Erro ao cadastrar usuário"; }
 
-                Snackbar snackbar = Snackbar.make(view, error, Snackbar.LENGTH_SHORT);
-                snackbar.setBackgroundTint(Color.WHITE);
-                snackbar.setTextColor(Color.BLACK);
+                Snackbar snackbar = Snackbar
+                        .make(view, error, Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(Color.WHITE)
+                        .setTextColor(Color.BLACK);
                 snackbar.show();
             }
         });
     }
 
-    private void saveUser() {
-
+    private void saveUser()
+    {
         String name = editName.getText().toString();
         String userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
         Map<String, Object> users = new HashMap<>();
@@ -101,7 +105,7 @@ public class FormCadastro extends AppCompatActivity {
         DocumentReference documentReference = firestore.collection("Users").document(userID);
 
         documentReference.set(users)
-                .addOnSuccessListener(unused -> Log.d("db", "Sucesso ao salvar dados"))
-                .addOnFailureListener(e -> Log.d("db_error", "Erro ao salvar dados" + e));
+                .addOnSuccessListener(unused -> Log.d("firestore", "Sucesso ao salvar dados"))
+                .addOnFailureListener(e -> Log.d("firestore_error", "Erro ao salvar dados" + e));
     }
 }
--- app/src/main/java/com/devd/projetofirebase/FormLogin.java
+++ app/src/main/java/com/devd/projetofirebase/FormLogin.java
@@ -3,69 +3,77 @@ package com.devd.projetofirebase;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
 
     private EditText editEmail, editPassword;
-    private Button button;
-    private TextView textCadastro;
+    private Button loginButton, registerButton;
     private ProgressBar progressBar;
-    String[] messages = { "Preencha todos os campos!" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-
         Objects.requireNonNull(getSupportActionBar()).hide();
+
         setComponents();
 
-        button.setOnClickListener(view -> {
-
+        loginButton.setOnClickListener(view ->
+        {
             String email = editEmail.getText().toString();
             String password = editPassword.getText().toString();
 
             if (email.isEmpty() || password.isEmpty())
             {
-                Snackbar snackbar = Snackbar.make(view, messages[0], BaseTransientBottomBar.LENGTH_SHORT);
-                snackbar.setBackgroundTint(Color.WHITE);
-                snackbar.setTextColor(Color.BLACK);
+                Snackbar snackbar = Snackbar
+                        .make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(Color.WHITE)
+                        .setTextColor(Color.BLACK);
                 snackbar.show();
-            } else authenticate();
+            }
+            else authenticate();
         });
 
-        textCadastro.setOnClickListener(view -> startActivity(new Intent(FormLogin.this, FormCadastro.class)));
+        registerButton.setOnClickListener(view -> startActivity(new Intent(FormLogin.this, FormCadastro.class)));
     }
 
-    private void authenticate() {
+    private void setComponents()
+    {
+        editEmail = findViewById(R.id.edt_email);
+        editPassword = findViewById(R.id.edt_password);
+        loginButton = findViewById(R.id.btn_login);
+        registerButton = findViewById(R.id.btn_register);
+        progressBar = findViewById(R.id.progressBar);
+    }
 
+    private void authenticate()
+    {
         String email = editEmail.getText().toString();
         String password = editPassword.getText().toString();
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(task ->
+        {
+            if (task.isSuccessful())
+            {
+                progressBar.setVisibility(View.VISIBLE);
+                new Handler().postDelayed((Runnable) this::startIntent, 3000);
             }
         });
     }
 
-    private void setComponents() {
-        textCadastro = findViewById(R.id.txt_cadastro);
-        editEmail = findViewById(R.id.edt_email);
-        editPassword = findViewById(R.id.edt_password);
-        button = findViewById(R.id.btn_entrar);
-        progressBar = findViewById(R.id.progressBar);
+    private void startIntent()
+    {
+        Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
+        startActivity(intent);
+        finish();
     }
 }
